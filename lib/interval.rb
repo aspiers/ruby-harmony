831a70bf21bcee20f11c8fdba28247a6d2b6d41e
Adam Spiers <music@adamspiers.org>
2013-08-10 11:36:00 +0100

    add Interval#long_name

--- lib/interval.rb
+++ lib/interval.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/integer/inflections'
+
 require 'scale_type'
 require 'accidental'
 
@@ -14,6 +16,37 @@ class Interval < Struct.new(:degree, :accidental)
 
   def name
     Accidental::LABELS[accidental] + degree.to_s
+  end
+
+  def adjective
+    return "aug" if accidental == 1
+
+    a =
+      case ((degree - 1) % 7) + 1
+      when 1
+        [nil, ""]
+      when 2, 3, 6, 7
+        %w(minor major)
+      when 4, 5
+        %w(dim perfect)
+      end
+
+    return a[accidental + 1] || 'unrecognised'
+  end
+
+  def long_name
+    case name
+    when "1"
+      "unison"
+    when "b9"
+      return "flat 9th"
+    when "#11"
+      return "sharp 11th"
+    when "b13"
+      return "flat 13th"
+    else
+      return adjective + ' ' + (degree == 1 ? "unison" : degree.ordinalize)
+    end
   end
 
   def Interval.by_name(name)
--- spec/interval_spec.rb
+++ spec/interval_spec.rb
@@ -3,42 +3,44 @@ require 'note'
 
 describe Interval do
   INTERVALS = [
-    [  1,  0,  "1", "C" , 0, "F#", 0, "Gb" , 0 ],
-    [  1, +1, "#1", "C#", 0, "Fx", 0, "G"  , 0 ],
+    [  1,  0,   "1", "C" , 0, "F#", 0, "Gb" , 0, "unison"           ],
+    [  1, +1,  "#1", "C#", 0, "Fx", 0, "G"  , 0, "aug unison" ],
 
-    [  2, -1, "b2", "Db", 0, "G" , 0, "Abb", 0 ],
-    [  2,  0,  "2", "D" , 0, "G#", 0, "Ab" , 0 ],
-    [  2, +1, "#2", "D#", 0, "Gx", 0, "A"  , 0 ],
+    [  2, -1,  "b2", "Db", 0, "G" , 0, "Abb", 0, "minor 2nd"        ],
+    [  2,  0,   "2", "D" , 0, "G#", 0, "Ab" , 0, "major 2nd"        ],
+    [  2, +1,  "#2", "D#", 0, "Gx", 0, "A"  , 0, "aug 2nd"          ],
 
-    [  3, -1, "b3", "Eb", 0, "A" , 0, "Bbb", 0 ],
-    [  3,  0,  "3", "E" , 0, "A#", 0, "Bb" , 0 ],
+    [  3, -1,  "b3", "Eb", 0, "A" , 0, "Bbb", 0, "minor 3rd"        ],
+    [  3,  0,   "3", "E" , 0, "A#", 0, "Bb" , 0, "major 3rd"        ],
 
-    [  4,  0,  "4", "F" , 0, "B" , 0, "Cb" , 0 ],
-    [  4, +1, "#4", "F#", 0, "B#", 1, "C"  , 1 ],
+    [  4,  0,   "4", "F" , 0, "B" , 0, "Cb" , 0, "perfect 4th"      ],
+    [  4, +1,  "#4", "F#", 0, "B#", 1, "C"  , 1, "aug 4th"          ],
 
-    [  5, -1, "b5", "Gb", 0, "C" , 1, "Dbb", 1 ],
-    [  5,  0,  "5", "G" , 0, "C#", 1, "Db" , 1 ],
-    [  5, +1, "#5", "G#", 0, "Cx", 1, "D"  , 1 ],
+    [  5, -1,  "b5", "Gb", 0, "C" , 1, "Dbb", 1, "dim 5th"          ],
+    [  5,  0,   "5", "G" , 0, "C#", 1, "Db" , 1, "perfect 5th"      ],
+    [  5, +1,  "#5", "G#", 0, "Cx", 1, "D"  , 1, "aug 5th"          ],
 
-    [  6, -1, "b6", "Ab", 0, "D" , 1, "Ebb", 1 ],
-    [  6,  0,  "6", "A" , 0, "D#", 1, "Eb" , 1 ],
-    [  6, +1, "#6", "A#", 0, "Dx", 1, "E"  , 1 ],
+    [  6, -1,  "b6", "Ab", 0, "D" , 1, "Ebb", 1, "minor 6th"        ],
+    [  6,  0,   "6", "A" , 0, "D#", 1, "Eb" , 1, "major 6th"        ],
+    [  6, +1,  "#6", "A#", 0, "Dx", 1, "E"  , 1, "aug 6th"          ],
 
-    [  7, -1, "b7", "Bb", 0, "E" , 1, "Fb" , 1 ],
-    [  7,  0,  "7", "B" , 0, "E#", 1, "F"  , 1 ],
+    [  7, -1,  "b7", "Bb", 0, "E" , 1, "Fb" , 1, "minor 7th"        ],
+    [  7,  0,   "7", "B" , 0, "E#", 1, "F"  , 1, "major 7th"        ],
 
-    [  9, -1, "b9", "Db", 1, "G" , 1, "Abb", 1 ],
-    [  9,  0,  "9", "D" , 1, "G#", 1, "Ab" , 1 ],
-    [  9,  1, "#9", "D#", 1, "Gx", 1, "A"  , 1 ],
+    [  9, -1,  "b9", "Db", 1, "G" , 1, "Abb", 1, "flat 9th"         ],
+    [  9,  0,   "9", "D" , 1, "G#", 1, "Ab" , 1, "major 9th"        ],
+    [  9,  1,  "#9", "D#", 1, "Gx", 1, "A"  , 1, "aug 9th"          ],
 
-    [ 11,  0, "11", "F" , 1, "B" , 1, "Cb" , 1 ],
-    [ 11,  1,"#11", "F#", 1, "B#", 2, "C"  , 2 ],
+    [ 11,  0,  "11", "F" , 1, "B" , 1, "Cb" , 1, "perfect 11th"     ],
+    [ 11,  1, "#11", "F#", 1, "B#", 2, "C"  , 2, "sharp 11th"       ],
 
-    [ 13, -1,"b13", "Ab", 1, "D" , 2, "Ebb", 2 ],
+    [ 13, -1, "b13", "Ab", 1, "D" , 2, "Ebb", 2, "flat 13th"        ],
 
   ]
 
-  shared_examples "an interval" do |degree, accidental, name, e1, o1, e2, o2, e3, o3|
+  shared_examples "an interval" do
+    |degree, accidental, name, e1, o1, e2, o2, e3, o3, long_name|
+
     let(:interval) { Interval.new(degree, accidental) }
     let(:c)        { Note.by_name('C')                }
     let(:f_sharp)  { Note.by_name('F#')               }
@@ -76,8 +78,12 @@ describe Interval do
       interval.from(g_flat).octave.should == o3
     end
 
-    it "should have the right name" do
+    it "should have the right (short) name" do
       interval.name.should == name
+    end
+
+    it "should have the right (long) name" do
+      interval.long_name.should == long_name
     end
 
     it "should be constructable by name" do
