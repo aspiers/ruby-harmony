57716f875b8ebf33c11895d511f6aedd1c59e409
Adam Spiers <music@adamspiers.org>
2013-08-11 11:38:10 +0100

    lib/mode_in_key.rb: add some docs

--- lib/mode.rb
+++ lib/mode.rb
@@ -20,15 +20,21 @@ class Mode
     @degrees ||= (1..(increments.size+1)).to_a.rotate(degree - 1)
   end
 
+  # Returns a NoteArray of the ascending notes of the mode, in the given key.
   def notes(key_note)
     NoteArray.new(degrees.map { |degree| scale_type.note(key_note, degree) })
   end
 
+  # Returns a NoteArray of the ascending notes of the mode, starting
+  # on the given note.  For example, if the mode is dorian and the
+  # given starting note is C, then it will return Bb major starting on
+  # C.
   def notes_from(starting_note)
     key_note = scale_type.key(starting_note, degree)
     notes(key_note)
   end
 
+  # Returns a ModeInKey instance representing this mode in the given key.
   def in_key(key_note)
     ModeInKey.new(self, key_note)
   end
