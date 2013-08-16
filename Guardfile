8f4d0dd2bce84c394b094a94916320e3ec1213ad
Adam Spiers <music@adamspiers.org>
2013-08-17 00:53:31 +0100

    autoload anything from spec/support

--- Guardfile
+++ Guardfile
@@ -17,6 +17,7 @@ end
 
 group :tests do
   guard 'rspec', guard_opts do
+    watch(%r{^spec/support/.+\.rb})
     watch(%r{^spec/.+_spec\.rb})
     watch(%r{^lib/(.+)\.rb}) { |m| "spec/#{m[1]}_spec.rb" }
     watch(%r{^lib/accidental\.rb}) { all }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
 RSpec.configure do |c|
   # declare an exclusion filter
   c.filter_run_excluding :broken => true
