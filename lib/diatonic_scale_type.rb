4e840df6d924d170b833a323c09adfae0122915b
Adam Spiers <music@adamspiers.org>
2014-02-23 12:23:15 +0000

    tidy up handling of mode names

--- lib/diatonic_scale_type.rb
+++ lib/diatonic_scale_type.rb
@@ -1,6 +1,8 @@
 require 'scale_type'
 
 class DiatonicScaleType < ScaleType
+  attr_accessor :mode_names
+
   def initialize(name, increments)
     super(name, increments, 7, 12)
   end
@@ -13,71 +15,45 @@ class DiatonicScaleType < ScaleType
   # DOUBLE_HARMONIC = new('dbl harm',        [ 1, 3, 1, 2, 1, 3, 1 ])
   # ENIGMATIC       = new('enigmatic',       [ 1, 3, 2, 2, 2, 2, 2 ])
 
-  class << MAJOR
-  #DEGREES = %w(ion dor phryg lyd mixo aeol loc)
-  DEGREES = %w(ionian dorian phrygian lydian mixo aeolian locrian)
-  #DEGREES = %w(ionian dorian phrygian lydian mixolydian aeolian locrian)
-
-    def mode_name(degree)
-      DEGREES[degree - 1]
-    end
+  def mode_name(degree)
+    mode_names[degree - 1]
   end
 
-  class << MELODIC_MINOR
-    def mode_name(degree)
-      case degree
-      when 2
-        "dorian b2"
-      when 3
-        "lydian augmented"
-      when 4
-        "lydian dominant"
-      when 5
-        "dominant b13"
-      when 6
-        "locrian natural 2"
-      when 7
-        "altered"
-      else
-        super
-      end
-    end
-  end
+  #MAJOR.mode_names = %w(ion dor phryg lyd mixo aeol loc)
+  MAJOR.mode_names = %w(ionian dorian phrygian lydian mixo aeolian locrian)
+  #MAJOR.mode_names = %w(ionian dorian phrygian lydian mixolydian aeolian locrian)
 
-  class << HARMONIC_MINOR
-    def mode_name(degree)
-      case degree
-      when 2
-        "locrian natural 6"
-      when 3
-        "major #5"
-      when 4
-        # "dorian / lydian"
-        "dorian #4"
-      when 5
-        "dominant b9 b13"
-      when 6
-        "lydian #2"
-      else
-        super
-      end
-    end
-  end
+  MELODIC_MINOR.mode_names = [
+    nil,                  # 1
+    # "phrygian natural 6", # 2
+    "dorian b2",          # 2
+    "lydian augmented",   # 3
+    "lydian dominant",    # 4
+    "dominant b13",       # 5
+    "locrian natural 2",  # 6
+    "altered",            # 7
+  ]
 
-  class << HARMONIC_MAJOR
-    def mode_name(degree)
-      case degree
-      when 4
-        # "lydian mel min"
-        "melodic min #4"
-      when 5
-        "dominant b9"
-      when 6
-        "lydian #2 #5"
-      else
-        super
-      end
-    end
-  end
+  HARMONIC_MINOR.mode_names = [
+    nil,                 # 1
+    "locrian natural 6", # 2
+    "major #5",          # 3
+    # "dorian / lydian",   # 4
+    "dorian #4",         # 4
+    "dominant b9 b13",   # 5
+    "lydian #2",         # 6
+    nil,                 # 7
+  ]
+
+  HARMONIC_MAJOR.mode_names = [
+    nil,                # 1
+    nil,                # 2
+    nil,                # 3
+    # "lydian mel min",   # 4
+    "melodic min #4",   # 4
+    "dominant b9",      # 5
+    "lydian #2 #5",     # 6
+    nil,                # 7
+  ]
 end
 
